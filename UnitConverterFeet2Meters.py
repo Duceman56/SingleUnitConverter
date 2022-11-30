9a0068b27122468ca169186e22fd884dd30b64b8
Duceman56 <114949617+Duceman56@users.noreply.github.com>
2022-11-30 13:46:15 -0600

    Update UnitConverterFeet2Meters.py

--- UnitConverterFeet2Meters.py
+++ UnitConverterFeet2Meters.py
@@ -1,11 +1,11 @@
 print("Feet to Meter Conversion")
 x = int(input("Input object length in feet: "))
-print("You entered: ") + (print(x))
+print("You entered: " + str(x))
+
 if x >= 0: 
     print("Acceptable number")
 else : 
     exit(print("ERROR! Positive value needed!"))
 
-converted_unit = (x/3.281)
-converted_round = round(converted_unit, 5)
-print(converted_round)
+converted_unit = round((x/3.281), 3)
+print(converted_unit)
