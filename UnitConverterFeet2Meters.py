829e259e0fcdd04924db6ce2072a3c4d1983efb0
Duceman56 <114949617+Duceman56@users.noreply.github.com>
2022-12-01 14:03:45 -0600

    Update UnitConverterFeet2Meters.py

--- UnitConverterFeet2Meters.py
+++ UnitConverterFeet2Meters.py
@@ -5,6 +5,8 @@ type = str(input("The units? (Ex. Miles, Meters, Inches, etc.): "))
 con_type = str(input("What units to be outputted?: "))
 print("you entered " + str(len) +" "+ str(type))
 
+capitalize(type)
+
 if type.__contains__("m") :
     print("meter")
 else :
