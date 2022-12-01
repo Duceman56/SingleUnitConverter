fcdd38445c9e7df8602c1f79a327bfc9012ddd8f
Duceman56 <114949617+Duceman56@users.noreply.github.com>
2022-12-01 11:51:27 -0600

    Update UnitConverterFeet2Meters.py

--- UnitConverterFeet2Meters.py
+++ UnitConverterFeet2Meters.py
@@ -1,16 +1,11 @@
-print("Feet to Meter Conversion")
-len = float(input("Input object length in feet: "))
-print("You entered: " + str(len))
-
-if len >= 0: 
-    print("Acceptable number")
-    converted_unit = round((len/3.281), 3)
-    print(str(converted_unit) + " meters")
-else : 
-    print("ERROR! Positive value needed!")
-
-
 print("All purpose linear unit convertor")
-type = str(input("What unit will be inputed? (Ex. Miles, Meters, Inches, etc.): "))
+
 len = float(input("Input length: "))
+type = str(input("The units? (Ex. Miles, Meters, Inches, etc.): "))
+con_type = str(input("What units to be outputted?: "))
 print("you entered " + str(len) +" "+ str(type))
+
+if type_contain_("m") :
+    print("meter")
+    else :
+        exit()
