94c2b3db4c2dacd780506bb6c6e3cd9c565962b6
Duceman56 <114949617+Duceman56@users.noreply.github.com>
2022-12-03 15:55:09 -0600

    Update GeneralUnitConvertor.py

--- GeneralUnitConvertor.py
+++ GeneralUnitConvertor.py
@@ -1,52 +1,30 @@
-METER_INCH = 39.37
-METER_FEET = 3.281
-INCH_FEET = 12
-METER_KM = 1000
-METER_DM = 10
+t = [
+    ["inch", (1 / 39.3701)],
+    ["foot", (1 / 3.28084)],
+    ["kilometer", (1000)],
+    ["anyUnit", 42069]
+]
 
-def convertToUnit (inputValue, inputUnit, outputUnit) :
-    upperInputUnit = inputUnit.upper()
-    if outputUnit.upper() == "DECIMETER" :
-        return(convertToUnit(inputValue * METER_DM, inputUnit,  "METER")) ##culprit of error
-    elif upperInputUnit == "METER" :
-        return(convertFromMeter(inputValue, outputUnit))  
-    elif upperInputUnit == "INCH" : 
-        return(convertFrominch(inputValue, outputUnit))
-    elif upperInputUnit == "FEET" :
-        return(convertFromFeet(inputValue, outputUnit))
-    elif upperInputUnit == "KILOMETER" :
-        return(convertFromMeter(inputValue * METER_KM, outputUnit))
-    elif upperInputUnit == "DECIMETER" : 
-        return(convertFromMeter(inputValue / METER_DM, outputUnit))
+def convertToUnit(inputValue, inputUnit, outputUnit) :
+    if inputUnit == "meter" :
+        return(meterToUnit(inputValue, outputUnit))
 
-def convertFromMeter (val, toConvert) :
-    if toConvert.upper() == "METER" :
-        return(val)
-    elif toConvert.upper() == "INCH" : 
-        return(val * METER_INCH)
-    elif toConvert.upper() == "FEET" :
-        return(val * METER_FEET)
+    else :
+        toMeterFactor = getToMeterFac(inputUnit)
+        convertedValue = inputValue * toMeterFactor
+        return(meterToUnit(convertedValue, outputUnit))
 
 
-def convertFrominch (val, toConvert) :
-    if toConvert.upper() == "METER" :
-        return(val / METER_INCH)
-    elif toConvert.upper() == "INCH" : 
-        return(val)
-    elif toConvert.upper() == "FEET" :
-        return(val / INCH_FEET)
+#precondition inputValue is guarentieed to be in meters
+def meterToUnit(inputValue, outputUnit) :
+    return(inputValue / getToMeterFac(outputUnit))
+
+def getToMeterFac(stringUnit) :
+     for row in t :
+        row[0]
+        if row[0] == stringUnit:
+            return(row[1])
 
 
-
-def convertFromFeet (val, toConvert) :
-    if toConvert.upper() == "METER" :
-        return(val / METER_FEET)
-    elif toConvert.upper() == "INCH" : 
-        return(val * INCH_FEET)
-    elif toConvert.upper() == "FEET" :
-        return(val)
-
-##seperateDriver
-
-x  = convertToUnit(3, "feet", "meter")
-print(x)
+z = convertToUnit(1, "kilometer", "inch")
+print(z)
